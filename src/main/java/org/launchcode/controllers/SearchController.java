71e87835b14f1b81eefb77021067ce34c9f14272
tspencer0917 <tns3327@truman.edu>
2019-08-26 18:38:07 -0500

    Implemented changes as specified in techjobs-mvc assignment

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,19 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value="results")
+    public String searchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+        ArrayList<HashMap<String, String>> searchResults = new ArrayList<>();
+        if(searchType.equals("all")){
+            searchResults = JobData.findByValue(searchTerm);
+        } else {
+            searchResults = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("resultCountMessage", searchResults.size() + " Result(s)");
+        model.addAttribute("jobs", searchResults);
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+    }
 
 }
